3c77d7df8de0bcb2ebb23128082275a6bdaf090e
iloewi <wegweik@gmail.com>
2025-07-16 18:21:10 +0530

    Delete leave-job-app directory

--- leave-job-app/components/Navbar.tsx
+++
@@ -1,7 +0,0 @@
-export default function Navbar() {
-  return (
-    <header className="bg-white shadow-sm py-4 px-6 sticky top-0 z-10">
-      <h1 className="text-xl font-semibold">📄 Application Generator</h1>
-    </header>
-  )
-}
